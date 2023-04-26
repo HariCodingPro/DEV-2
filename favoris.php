3aa4a8f9fda845ef58285edd7bd0df7fbae6ca71
Hari <104674926+HariCodingPro@users.noreply.github.com>
2023-04-26 13:21:32 +0200

    Add files via upload

--- favoris.php
+++ favoris.php
@@ -5,7 +5,61 @@
     if($_SESSION["connexion"]==0){
         header('Location: connexion.php');
        }
+
+
+
+    for($i=0;$i<$_SESSION["beber"];$i++){
+      
+        if(isset($_POST["csubmit".$i])){
+            echo $_POST["id".$i];
+            $tab=array(); 
+            //$new_favoris=array();               
+            $sql = "SELECT favoris FROM Utilisateur WHERE id='".$_SESSION["idUser"]."'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows> 0) {
+
+                while($row = $result->fetch_assoc()) {
+                    $favoris=$row["favoris"];
+                }
+            
+            } 
+
+            else {
+                echo "0 résultat";
+            }
+            
+            $tab=explode(",",$favoris);
+            //var_dump($tab);
+            for($n=0; $n<count($tab);$n++){
+               
+                if($_POST["id".$i]==$tab[$n]){
+                    unset($tab[$n]);
+                    break;
+                }               
+            }
+            $new_fav=implode(",",$tab);
+           
+
+            $sql = "UPDATE Utilisateur SET favoris='".$new_fav."' WHERE id='".$_SESSION["idUser"]."'";
+            $result = $conn->query($sql);
+        
+
+                if ($result == TRUE) {
+                    echo "everyting is ok";
+                } 
+            
+                else {
+                    echo "Error: <br>" . $conn->error;
+                }
+                break;
+        }
+       
+    }
+
+
     
+
     ?>
     <body>
         <?php include_once("header.php"); ?>
@@ -23,7 +77,7 @@
 
                     while($row = $result->fetch_assoc()) {
                         $favoris=$row["favoris"];
-                        
+        
                     }
                 
                 } 
@@ -34,13 +88,12 @@
 
                 $tab=explode(",",$favoris);
                 $taille = sizeof($tab);
+                $count=0;
 
                 for($i=0; $i<$taille; $i++) {
                 $sql = "SELECT * FROM Animal WHERE id='".$tab[$i]."'";
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
-                    
-
                 // output data of each row
                     while($row = $result->fetch_assoc()) {
 
@@ -49,7 +102,6 @@
 
                             
                             echo "
-
                             <div class='container_fav'>
                                 <div class='liste_favoris_item'>
                                     <div class='texte_favoris'> <a href='pageanimal.php?esp=".$row["idEspece"]."&race=".$row["idRace"]."&id=".$row["id"]."' id='deux'>
@@ -57,10 +109,13 @@
                                         <h4 style='text-decoration:underline'>Favoris $i:</h4>
                                         <p style='color:black;'>".$row["nom"]."</p>
                                     </div>
-
                                     <div class='bouton-container'>
-                                        <a href='pageanimal.php?esp=".$row["idEspece"]."&race=".$row["idRace"]."&id=".$row["id"]."'><input type='submit' class='bouton_supprimer' name='csubmit' value='Consulter la publication'></a><br><br>
-                            
+                                       
+                                            <a href='pageanimal.php?esp=".$row["idEspece"]."&race=".$row["idRace"]."&id=".$row["id"]."'><input type='submit' class='bouton_consulter' name='csubmit' value='Consulter la publication'></a><br><br>
+                                            <form method='POST'>
+                                            <input type='hidden' name='id".$count."' value='".$row["id"]."'>
+                                            <input type='submit' class='bouton_supprimer' name='csubmit".$count."' value='Supprimer'> 
+                                        </form>
                                     </div>
                                 </div>
                             </div>
@@ -69,16 +124,29 @@
 
                         }
                         else{
-                            echo "erreur dossier image!";
+                            echo "
+                                <p>erreur dossier image!</p>
+                                ";
                         }
 
+                        
+                        $count++;
+                        $_SESSION['beber']=$count;
+                        
                     }
                 }    
-
-                else{
-                    echo "vide";
+            }                     
+                if ($taille<=1){
+                    echo"                   
+                        <div class='pasfavoris'>
+                            Auncun favoris
+                            <a href='liste_description.php'><button>Consulter annonces</button></a>
+                            <img src='images/favoris.jpg' id='chattriste'>
+                        </div> 
+                     
+                    ";
                 }
-                }
+            
                 
             ?>
 
